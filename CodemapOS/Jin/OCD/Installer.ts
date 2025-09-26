282382dd01e9cb2bd80d1fcad3a4c3e46e1a92a8
Randall lujan <meta2graei@gmail.com>
2025-09-26 15:01:03 -0700

    Create Installer.ts

---
+++ CodemapOS/Jin/OCD/Installer.ts
@@ -0,0 +1,6 @@
+export function installCodemap() {
+  Jin.order("altar.summon");
+  Jin.command("altar.summon", "build");
+  Jin.command("altar.summon", "run");
+  Jin.demand("vault.echo", "protect");
+}
